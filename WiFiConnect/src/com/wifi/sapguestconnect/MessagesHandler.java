18d73f8b2d46a6af9b95d8619137a8488f8c52c2
dagrest@gmail.com <dagrest@gmail.com@9e54c3e2-c9f6-6806-df01-97cac6f6c83c>
2010-07-28 17:21:58 +0000

    Fix of the crash appearing during starting of the application in landscape screen orientation.

--- WiFiConnect/src/com/wifi/sapguestconnect/MessagesHandler.java
+++ WiFiConnect/src/com/wifi/sapguestconnect/MessagesHandler.java
@@ -27,24 +27,36 @@ public class MessagesHandler extends Handler {
 		super.handleMessage(msg);
 		try {
 			wifiActivity.fillLoginDataDialog();
+			switch (msg.what) {
+				case 0: wifiActivity.setLogMessage(errorMessages.NOT_CONNECTED);
+						break;
+				case 1: wifiActivity.setLogMessage(errorMessages.ALREADY_CONNECTED);
+						break;
+				case 2: wifiActivity.setLogMessage(errorMessages.SUCCESS);
+						break;
+				case 3: wifiActivity.setLogMessage(errorMessages.FAILED);
+						break;
+				case 4: wifiActivity.setLogMessage(errorMessages.WIFI_TURNED_OFF);
+						break;
+				case 5: wifiActivity.setLogMessage(errorMessages.NOT_CORRECT_WIFI);
+						break;
+			}
+			progressDialog.dismiss();
 		} catch (Exception e) {
+			// This catch will "fix" the folllwoing problem as well:
+			// Exception: view not attached to window manager ...
+			// The problem is related to handling of screen orientation change 
+			// when progress dialog and background thread active
+			// See the following link:
+			// http://stackoverflow.com/questions/1111980/how-to-handle-screen-orientation-change-when-progress-dialog-and-background-threa
+			// Citation:
+			// When you switch orientations, Android will create a new View. You're probably 
+			// getting crashes because your background thread is trying to change the state on the old one. 
+			// (It may also be having trouble because your background thread isn't on the UI thread)
+			// I'd suggest making that mHandler volatile and updating it when the orientation changes.
+			
 			logHelper.toLog(isLogEnabled, "EXCEPTION: MessagesHandler -> handleMessage(): " + e.getMessage());
 		}
-		switch (msg.what) {
-			case 0: wifiActivity.setLogMessage(errorMessages.NOT_CONNECTED);
-					break;
-			case 1: wifiActivity.setLogMessage(errorMessages.ALREADY_CONNECTED);
-					break;
-			case 2: wifiActivity.setLogMessage(errorMessages.SUCCESS);
-					break;
-			case 3: wifiActivity.setLogMessage(errorMessages.FAILED);
-					break;
-			case 4: wifiActivity.setLogMessage(errorMessages.WIFI_TURNED_OFF);
-					break;
-			case 5: wifiActivity.setLogMessage(errorMessages.NOT_CORRECT_WIFI);
-					break;
-		}
-		progressDialog.dismiss();
 		logHelper.toLog(isLogEnabled, "MessagesHandler -> handleMessage() ended.");
 	}
 }
