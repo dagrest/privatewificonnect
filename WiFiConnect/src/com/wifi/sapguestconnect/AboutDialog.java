8431e14f8c6f77262a8e860ffefbe44bf2fbdd66
georgehdd <georgehdd@9e54c3e2-c9f6-6806-df01-97cac6f6c83c>
2011-01-02 21:43:06 +0000

    Minor refactoring for the About dialog and menu

--- WiFiConnect/src/com/wifi/sapguestconnect/AboutDialog.java
+++ WiFiConnect/src/com/wifi/sapguestconnect/AboutDialog.java
@@ -1,6 +1,30 @@
 package com.wifi.sapguestconnect;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 public class AboutDialog 
 {
-	
+	public static void show(Context context)
+	{
+		Resources resources = context.getResources();
+		Dialog dialog = new Dialog(context);
+
+		// Set Title
+		dialog.setContentView(R.layout.about_dialog);
+		dialog.setTitle(resources.getString(R.string.app_name));
+
+		// Set Text
+		TextView text = (TextView) dialog.findViewById(R.id.text);
+		text.setText(resources.getString(R.string.app_about_summary));
+		
+		// Set Image
+		ImageView image = (ImageView) dialog.findViewById(R.id.image);
+		image.setImageResource(R.drawable.sap_connect);
+		
+		dialog.show();
+	}
 }
